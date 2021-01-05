7c77c7e8eeddd734728d97d72323110733f32013
Ben Shukrallah <benjamin.shukrallah@live.longwood.edu>
2021-01-04 20:49:01 -0500

    lots of changes

--- initial_env.sh
+++ initial_env.sh
@@ -7,14 +7,21 @@ sudo cp terraform /usr/local/bin/
 sudo yum install python3 -y
 pip3 install awscli --user
 pip3 install ansible --user
+pip3 install boto3 --user
+
 mkdir deploy_iac_tf_ansible
-wget https://raw.githubusercontent.com/linuxacademy/content-deploying-to-aws-ansible-terraform/master/aws_la_cloudplayground_version/ansible.cfg
+mdkir deploy_iac_tf_ansible/ansible_templates
+mkdir deploy_iac_tf_ansible/ansible_templates/inventory_aws/
+wget https://raw.githubusercontent.com/bshukrallah/terraform/master/ansible.cfg
 wget https://raw.githubusercontent.com/bshukrallah/terraform/master/providers.tf
 wget https://raw.githubusercontent.com/bshukrallah/terraform/master/networks.tf
 wget https://raw.githubusercontent.com/bshukrallah/terraform/master/variables.tf
 wget https://raw.githubusercontent.com/bshukrallah/terraform/master/security_groups.tf
 wget https://raw.githubusercontent.com/bshukrallah/terraform/master/outputs.tf
 wget https://raw.githubusercontent.com/bshukrallah/terraform/master/instances.tf
+wget https://raw.githubusercontent.com/bshukrallah/terraform/master/ansible_templates/jenkins-master-sample.yml
+wget https://raw.githubusercontent.com/bshukrallah/terraform/master/ansible_templates/jenkins-worker-sample.yml
+wget https://raw.githubusercontent.com/bshukrallah/terraform/master/ansible_templates/inventory_aws/tf_aws_ec2.yml
 mv ansible.cfg deploy_iac_tf_ansible/ansible.cfg
 mv providers.tf deploy_iac_tf_ansible/providers.tf
 mv networks.tf deploy_iac_tf_ansible/networks.tf
@@ -22,6 +29,11 @@ mv variables.tf deploy_iac_tf_ansible/variables.tf
 mv security_groups.tf deploy_iac_tf_ansible/security_groups.tf
 mv instances.tf deploy_iac_tf_ansible/instances.tf
 mv outputs.tf deploy_iac_tf_ansible/outputs.tf
+mv jenkins-master-sample.yml deploy_iac_tf_ansible/ansible_templates/jenkins-master-sample.yml
+mv jenkins-worker-sample.yml deploy_iac_tf_ansible/ansible_templates/jenkins-worker-sample.yml
+mv tf_aws_ec2.yml deploy_iac_tf_ansible/ansible_templates/inventory_aws/tf_aws_ec2.yml
+
+
 
 echo "Generating Key for AWS Instances"
 
